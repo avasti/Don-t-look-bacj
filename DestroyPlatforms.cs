5f18717470e256ed11af6fcaafbaf8a176c828ba
avasti <frederic.marimon.serra@gmail.com>
2016-04-12 18:02:09 +0200

    Addo

---
+++ DestroyPlatforms.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyPlatforms : MonoBehaviour {
+
+    public float forceAmount = 1000f;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "valla")
+        {
+            rb.AddForce(-transform.forward*forceAmount, ForceMode.Acceleration);
+            rb.useGravity = true;
+        }
+    }
+}
