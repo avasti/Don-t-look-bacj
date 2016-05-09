eeb25e106ebf5a42df520eecc8b0e64112da3255
KyuRicard <ricardboscholivar@gmail.com>
2016-05-09 11:43:42 +0200

    Player Movement fixed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,13 +8,17 @@ public class PlayerMovement : MonoBehaviour {
     public float jumpSpeed = 500;
     public Image img;
     public Sprite empty;
+    public Text text;
+    public Sprite speedUp, speedDown;
 
+    Animator anim;
     float copyMaxSpeed;
     double time;
     bool isGround = false;
 	// Use this for initialization
 	void Start () {
         copyMaxSpeed = maxSpeed;
+        anim = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -22,9 +26,16 @@ public class PlayerMovement : MonoBehaviour {
         transform.Translate(new Vector3(Time.deltaTime * 2 * speed, 0, 0));
         if (Input.GetAxis("Jump") != 0 && isGround)
         {
+            anim.SetBool("Jump", true);
             GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpSpeed, 0));
             isGround = false;
         }
+        else
+        {
+            anim.SetBool("Jump", false);
+        }
+        anim.SetBool("Sprint", maxSpeed == 6);
+        anim.SetBool("Ski", maxSpeed == 5);
         if (time <= 0)
         {
             maxSpeed = copyMaxSpeed;
@@ -37,14 +48,11 @@ public class PlayerMovement : MonoBehaviour {
         }
 		speed += Time.deltaTime;
         speed = Mathf.Min(speed, maxSpeed);
+        text.text = "Score: " + Assets.Scripts.ScoreManager.Score;
 	}
 
-    void OnCollisionEnter(Collision other)
+    void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "Terra")
-        {
-            isGround = true;
-        }
         if (other.gameObject.name == "EndCube")
         {
             transform.position = new Vector3(0, transform.position.y, 0);
@@ -53,14 +61,40 @@ public class PlayerMovement : MonoBehaviour {
         if (other.gameObject.tag == "Speed Up")
         {
             maxSpeed = 6;
-            time = 2;
+            time += 2;
+            img.sprite = speedUp;
             Destroy(other.gameObject);            
         }
         if (other.gameObject.tag == "Speed Down")
         {
-            maxSpeed = 3.2f;
-            time = 1.3;
+            maxSpeed = 3f;
+            img.sprite = speedDown;
+            time += 2;
             Destroy(other.gameObject);
+        }
+        if (other.gameObject.tag == "Coin")
+        {
+            Assets.Scripts.ScoreManager.Score += 15;
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.tag == "Flag")
+        {
+            Assets.Scripts.ScoreManager.SaveScore();
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Dead");
+        }
+        if (other.gameObject.tag == "Ski")
+        {
+            maxSpeed = 5;
+            time = 3;
+            Destroy(other.gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGround = true;
         }
     }
 }
