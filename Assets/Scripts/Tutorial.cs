f7fd6546ead66fa5e63869b3c9f6232ff10ee4ef
KyuRicard <ricardboscholivar@gmail.com>
2016-06-12 21:13:29 +0200

    Add script for tutorial

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour {
+    float time = 2000;
+    string[] lines;
+    int lineCount = 0;
+	void Start () {
+        GetComponent<Text>().text = lines[0];
+	}
+
+	void Update () {
+        time -= Time.deltaTime;
+        if (time <= 0 && lineCount < lines.Length - 1)
+        {
+            ++lineCount;
+            GetComponent<Text>().text = lines[lineCount];
+            time += 2000;
+        }
+        if (time <= 0 && lineCount == lines.Length)
+        {
+            enabled = false;
+        }
+	}
+}
